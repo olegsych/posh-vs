98c6dd183e51023f398e6b37b7f1a2831dc0d304
Oleg Sych <oleg_sych@hotmail.com>
2015-12-13 11:44:36 -0800

    Initial implementation of the posh-vs module

---
+++ posh-vs.psm1
@@ -0,0 +1,80 @@
+function Import-EnvironmentVariables {
+    [CmdletBinding()] param (
+        [Parameter(Mandatory = $true)] [string] $batchFile
+    )
+
+    if (-not (Test-Path $batchFile)) {
+        Throw "Batch file '$batchFile' does not exist."
+    }
+
+    Write-Verbose "Executing '$batchFile' to capture environment variables it sets."
+    cmd /c "`"$batchFile`" > nul & set" | ForEach-Object {
+        if ($_ -match "^(.+?)=(.*)$") {
+            [string] $variable = $matches[1]
+            [string] $value = $matches[2]
+            Write-Verbose "`$env:$variable=$value"
+            Set-Item -Force -Path "env:\$variable" -Value $value
+        }
+    }
+}
+
+function Import-VisualStudioEnvironment {
+    if (-not $env:VS140COMNTOOLS) {
+        Throw "Unable to determine location of Visual Studio 2015. The VS140COMNTOOLS environment is not set."
+    }
+
+    [string] $batchFile = (Join-Path $env:VS140COMNTOOLS "VsDevCmd.bat")
+    Import-EnvironmentVariables $batchFile
+}
+
+[string] $importModulePattern = "Import-Module.*posh-vs"
+[string] $importEnvironmentPattern = "Import-VisualStudioEnvironment"
+
+function Install-PoshVs {
+    [string[]] $profileScript = @()
+    [bool] $importModule = $true
+    [bool] $importEnvironment = $true
+    if (Test-Path $profile) {
+        $profileScript = Get-Content $profile
+        foreach ($line in $profileScript) {
+            if ($line -match $importModulePattern) {
+                $importModule = $false
+            }
+            if ($line -match $importEnvironmentPattern) {
+                $importEnvironment = $false
+            }
+        }
+    }
+
+    if ($importModule) {
+        $profileScript += "Import-Module posh-vs"
+    }
+
+    if ($importEnvironment) {
+        $profileScript += "Import-VisualStudioEnvironment" 
+    }
+    
+    $profileScript | Out-File $profile
+
+    Write-Host "Successfully added posh-vs to profile '$profile'."
+    Write-Host "Reload your profile for the changes to take effect:"
+    Write-Host "    . `$profile"
+}
+
+function Uninstall-PoshVs {
+    if (Test-Path $profile) {
+        [string[]] $script = Get-Content $profile
+        $script | Where-Object { 
+            -not ($_ -match $importModulePattern) -and
+            -not ($_ -match $importEnvironmentPattern)
+        } | Out-File $profile
+    }
+    
+    Write-Host "Successfully removed posh-vs from profile '$profile'."
+    Write-Host "Restart PowerShell for the changes to take effect."
+}
+
+Export-ModuleMember -Function Import-EnvironmentVariables
+Export-ModuleMember -Function Import-VisualStudioEnvironment
+Export-ModuleMember -Function Install-PoshVs
+Export-ModuleMember -Function Uninstall-PoshVs
---
+++ posh-vs.tests.ps1
@@ -0,0 +1,228 @@
+Describe "posh-vs" {
+    Import-Module .\posh-vs.psm1
+
+    function DeleteFile([string] $file) {
+        if (Test-Path $file) {
+            Remove-Item $file
+        }
+    }
+
+    function RenameFile([string] $file, [string] $newFile) {
+        if (Test-Path $file) {
+            Rename-Item -Path $file -NewName $newFile
+        }
+    }
+
+    function BackupProfile {
+        [string] $backup = ($profile + ".bak")
+        DeleteFile $backup
+        RenameFile $profile $backup
+    }
+
+    function RestoreProfile {
+        DeleteFile $profile
+        RenameFile ($profile + ".bak") $profile
+    }
+
+	Context "Import-EnvironmentVariables" {
+        [string] $batchFile
+        [string] $variable
+        [string] $value 
+
+        BeforeEach {
+            $variable = [guid]::NewGuid()
+            $value = "$([guid]::NewGuid())=$([guid]::NewGuid())"
+            $batchFile = Join-Path $env:TEMP ([IO.Path]::GetRandomFileName() + ".bat")
+        }
+
+		It "Invokes specified batch file and extracts environment variables it sets" {
+            "set $variable=$value" | Out-File $batchFile -Encoding ascii
+            
+            Import-EnvironmentVariables -batchFile $batchFile
+            
+            (Get-Item "env:$variable").Value | Should Be $value
+		}
+
+        It "Ignores output of batch file itself to avoid mistaking its output for actual environment variables" {
+            "echo $variable=$value"  | Out-File $batchFile -Encoding ascii
+
+            Import-EnvironmentVariables -batchFile $batchFile 
+            
+            Test-Path "env:$variable" | Should Be $false
+        }
+
+        It "Throws descriptive exception when specified batch file does not exist" {
+            { Import-EnvironmentVariables -batchFile $batchFile } | Should Throw "Batch file '$batchFile' does not exist."
+        }
+
+        It "Writes verbose message with details about the batch file" {
+            "" | Out-File $batchFile -Encoding ascii
+            Mock Write-Verbose -ModuleName posh-vs
+
+            Import-EnvironmentVariables -batchFile $batchFile
+
+            Assert-MockCalled Write-Verbose -ParameterFilter { $message -eq "Executing '$batchFile' to capture environment variables it sets." } -ModuleName posh-vs 
+        }
+
+        It "Writes verbose message with details about imported environment variables" {
+            "set $variable=$value" | Out-File $batchFile -Encoding ascii
+            Mock Write-Verbose -ModuleName posh-vs
+
+            Import-EnvironmentVariables -batchFile $batchFile
+
+            Assert-MockCalled Write-Verbose -ParameterFilter { $message -eq "`$env:$variable=$value" } -ModuleName posh-vs 
+        }
+
+        AfterEach {
+            if (Test-Path $batchFile) {
+                Remove-Item $batchFile
+            }
+
+            if (Test-Path "env:$variable") {
+                Remove-Item "env:$variable"
+            }
+        }
+	}
+
+    Context "Import-VisualStudioEnvironment" {
+        [string] $originalPath
+
+        BeforeEach {
+            $originalPath = $env:VS140ComnTools
+        }
+
+        It "Invokes Import-EnvironmentVariables with VS2015 VsDevCmd.bat" {
+            $env:VS140ComnTools = Join-Path $env:TEMP ([IO.Path]::GetRandomFileName())
+            Mock Import-EnvironmentVariables -ModuleName posh-vs
+
+            Import-VisualStudioEnvironment
+
+            [string] $expectedBatchFile = (Join-Path $env:VS140ComnTools "VsDevCmd.bat") 
+            Assert-MockCalled Import-EnvironmentVariables -ParameterFilter { $batchFile -eq $expectedBatchFile } -ModuleName posh-vs
+        }
+
+        It "Throws descriptive exception when VS140COMNTOOLS environment variable is not set" {
+            Remove-Item "env:\VS140COMNTOOLS"
+            { Import-VisualStudioEnvironment } | Should Throw "Unable to determine location of Visual Studio 2015. The VS140COMNTOOLS environment is not set."
+        }
+
+        AfterEach {
+            $env:VS140ComnTools = $originalPath
+        }
+    }
+
+    Context "Install-PoshVs" {
+
+        BeforeEach {
+            Mock Write-Host -ModuleName posh-vs
+            BackupProfile
+        }
+
+        It "Appends Import-Module and Import-VisualStudioEnvironment commands to existing profile script" {
+            [string] $existingScript = "Write-Host Foo"
+            New-Item -Path $profile -ItemType File -Value $existingScript
+
+            Install-PoshVs
+
+            Get-Content $profile | Should Be @(
+                $existingScript,
+                "Import-Module posh-vs",
+                "Import-VisualStudioEnvironment"
+            )
+        }
+
+        It "Creates new profile script if necessary" {
+            Install-PoshVs
+
+            Get-Content $profile | Should Be @(
+                "Import-Module posh-vs",
+                "Import-VisualStudioEnvironment"
+            )
+        }
+
+        It "Doesn't duplicate Import-Module command if profile script already contains it" {
+            @(
+                "  Import-Module   posh-vs"
+            ) | Out-File $profile
+
+            Install-PoshVs
+
+            Get-Content $profile | Should Be @(
+                "  Import-Module   posh-vs",
+                "Import-VisualStudioEnvironment"
+            )
+        }
+
+        It "Doesn't duplicate Import-VisualStudioEnvironment command if profile already contains it" {
+            @(
+                "  Import-VisualStudioEnvironment"
+            ) | Out-File $profile
+
+            Install-PoshVs
+
+            Get-Content $profile | Should Be @(
+                "Import-Module posh-vs",
+                "  Import-VisualStudioEnvironment"
+            )
+        }
+
+        It "Writes to host to explain what's going on" {
+            Install-PoshVs
+
+            Assert-MockCalled Write-Host -ParameterFilter { $object -eq "Successfully added posh-vs to profile '$profile'." } -ModuleName posh-vs
+            Assert-MockCalled Write-Host -ParameterFilter { $object -eq "Reload your profile for the changes to take effect:" } -ModuleName posh-vs
+            Assert-MockCalled Write-Host -ParameterFilter { $object -eq "    . `$profile" } -ModuleName posh-vs
+        }
+
+        AfterEach {
+            RestoreProfile
+        }
+    }
+
+    Context "Uninstall-PoshVs" {
+
+        BeforeEach {
+            Mock Write-Host -ModuleName posh-vs
+            BackupProfile
+        }
+
+        It "Removes Import-Module from profile script" {
+            "    Import-Module   posh-vs   " | Out-File $profile
+
+            Uninstall-PoshVs
+
+            Get-Content $profile | Should BeNullOrEmpty
+        }
+
+        It "Removes Import-VisualStudioEnvironment from profile script" {
+            "    Import-VisualStudioEnvironment   " | Out-File $profile
+
+            Uninstall-PoshVs
+
+            Get-Content $profile | Should BeNullOrEmpty
+        }
+
+        It "Preserves code unrelated to posh-vs" {
+            [string[]] $expected = @( "Write-Host Foo")
+            $expected | Out-File $profile
+            
+            Uninstall-PoshVs
+            
+            [string[]] $actual = Get-Content $profile 
+            $actual | Should Be $expected
+        }
+
+        It "Writes to host to explain what's going on" {
+            Uninstall-PoshVs
+
+            Assert-MockCalled Write-Host -ParameterFilter { $object -eq "Successfully removed posh-vs from profile '$profile'." } -ModuleName posh-vs
+            Assert-MockCalled Write-Host -ParameterFilter { $object -eq "Restart PowerShell for the changes to take effect." } -ModuleName posh-vs
+        }
+
+        AfterEach {
+            RestoreProfile
+        }
+    }
+
+    Remove-Module posh-vs
+}
