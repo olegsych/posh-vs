747373d8975055ad9386daaf9c145c3cc8e50547
Oleg Sych <oleg_sych@hotmail.com>
2015-12-13 13:46:16 -0800

    Added doc comments to address PSProvideCommentHelp analyzer warnings.

--- posh-vs.psm1
+++ posh-vs.psm1
@@ -1,6 +1,9 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Scope="Function", Target="*-PoshVS", Justification="PoshVs is a singular noun")]        
 param()
 
+<# .SYNOPSIS 
+Executes a batch file and copies environment variables it sets to the current
+PowerShell session #>
 function Import-BatchEnvironment {
     [CmdletBinding()] param (
         [Parameter(Mandatory = $true)] [string] $batchFile
@@ -21,6 +24,9 @@ function Import-BatchEnvironment {
     }
 }
 
+<# .SYNOPSIS
+Executes Visual Studio 2015's VsDevCmd.bat and copies environment variables it sets
+to the current PowerShell session. #>
 function Import-VisualStudioEnvironment {
     if (-not $env:VS140COMNTOOLS) {
         Throw "Unable to determine location of Visual Studio 2015. The VS140COMNTOOLS environment is not set."
@@ -33,6 +39,8 @@ function Import-VisualStudioEnvironment {
 [string] $importModulePattern = "Import-Module.*posh-vs"
 [string] $importEnvironmentPattern = "Import-VisualStudioEnvironment"
 
+<# .SYNOPSIS
+Adds Import-Module and Import-VisualStudioEnvironment commands to the current PowerShell profile script. #>
 function Install-PoshVs {
     [string[]] $profileScript = @()
     [bool] $importModule = $true
@@ -64,6 +72,8 @@ function Install-PoshVs {
     Write-Output "    . `$profile"
 }
 
+<# .SYNOPSIS 
+Removes Import-Module and Import-VisualStudioEnvironment from the current PowerShell profile script. #>
 function Uninstall-PoshVs {
     if (Test-Path $profile) {
         [string[]] $script = Get-Content $profile
