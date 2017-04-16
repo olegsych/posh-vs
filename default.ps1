2fa7ce4cefb7eb15c81a9f447a24f1039e25c3bd
Oleg Sych <olegsych@users.noreply.github.com>
2017-04-16 14:37:16 -0700

    Make default.ps1 generate manifest from AppVeyor build version.

--- default.ps1
+++ default.ps1
@@ -1,14 +1,38 @@
 Task default -Depends Build, Test
 
-Task Build {
-    Invoke-ScriptAnalyzer .\src\posh-vs.psm1
-    Test-ModuleManifest .\src\posh-vs.psd1
+Properties {
+    $moduleVersion = if ($env:APPVEYOR) { $env:APPVEYOR_BUILD_VERSION } else { '0.0.0' }
 }
+
+Task Clean {
+    Remove-Item .\out -Recurse -Force
+    New-Item -ItemType Directory out | Write-Verbose
+}
+
+Task BuildScript -Depends Clean {
+    Invoke-ScriptAnalyzer .\src\posh-vs.psm1
+    Copy-Item .\src\posh-vs.psm1 -Destination .\out\
+}
+
+Task BuildManifest -Depends Clean {
+    $source = Get-Content .\src\posh-vs.psd1
+
+    $expanded = @()
+    foreach ($line in $source) {
+        $expanded += $ExecutionContext.InvokeCommand.ExpandString($line)
+    }
+
+    $expanded | Out-File .\out\posh-vs.psd1
+
+    Test-ModuleManifest .\out\posh-vs.psd1 | Write-Verbose
+}
+
+Task Build -Depends BuildScript, BuildManifest
 
 Task Test {
     Invoke-Pester -Script .\test\posh-vs.tests.ps1 -EnableExit
 }
 
 Task Publish {
-     Publish-Module -Path .\ -NuGetApiKey $env:PowerShellApiKey
+     Publish-Module -Path .\out\ -NuGetApiKey $env:PowerShellApiKey
 }
